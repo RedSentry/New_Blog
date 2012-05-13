03fc3929250d83b8608db40e3fdceeace8da5ecd
The Creative One <thecreativeone@The-Creative-Ones-MacBook-Pro.local>
2012-05-13 13:56:37 +0800

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cf617b410d41217f69dfda677b928683142e47ca35dd29afe3e348f4e491c1295b1a2865d1b798cf875940d132273503ca5f3b1b256c02137a2132504ad6280a'
